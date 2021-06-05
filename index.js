fafe0357b04a46ca85a90c00b90444c2844d022f
Pia Torain <piatorain@yahoo.com>
2021-06-05 12:39:05 -0700

    Final Version
    Completed responsiveness and checked for errors.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function toggleTheme() {
     window.theme = typeof window.theme === "string" ? window.theme : "light";
-    var switchToTheme = window.theme === "light" ? "dark" : "light";
+    let switchToTheme = window.theme === "light" ? "dark" : "light";
     window.theme = switchToTheme;
     document.querySelector("body").setAttribute("theme", switchToTheme);
   }
   
+
+  //Function to change the theme from light to dark.
+    // Set the default: The theme is strictly  a string and the value is the current value(light or dark).
+        // If that is not true, then the theme is light.
+    // If the window theme is strictly light then switch to dark theme when the toggle is moved, otherwise the theme remains light.
+    // Finally, set the theme to the value of switchToTheme
